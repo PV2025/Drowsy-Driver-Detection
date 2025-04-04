72f0e050736c6b1bb3fa66ba36809ee391dfd8cf
PV2025 <165400301+PV2025@users.noreply.github.com>
2025-04-04 14:00:15 +0530

    Update steering_entropy.py

--- code/steering_entropy.py
+++ code/steering_entropy.py
@@ -1,12 +1,22 @@
-# Steering Entropy logic placeholder
+"""
+Module: Steering Entropy
+Calculates entropy of steering angle behavior over a time window.
+"""
 
 import numpy as np
 
 def compute_steering_entropy(steering_angles, bins=10):
     """
-    Calculate entropy based on histogram of steering angle values.
+    Computes entropy of steering angle variations.
+    
+    Args:
+        steering_angles (list or array): Recorded angles
+        bins (int): Number of histogram bins
+    
+    Returns:
+        float: Entropy score
     """
     hist, _ = np.histogram(steering_angles, bins=bins, density=True)
-    hist = hist[hist > 0]  # Remove zero bins
+    hist = hist[hist > 0]
     entropy = -np.sum(hist * np.log2(hist))
     return entropy
